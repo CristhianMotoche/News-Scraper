2b7971274c274f3f593e25d3844b2377cc4a911f
CristhianMotoche <CristhianMotoche@users.noreply.github.com>
2015-09-06 21:01:23 -0500

    First commit

---
+++ __main__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from src.website import Website
+from src.scrapers.hipertextualscraper import HipertextualScraper
+from src.scrapers.unoceroscraper import UnoCeroScraper
+from src.scrapers.slashdotscraper import  SlashDotScraper
+
+import sys
+
+websites = [ Website("Hipertextual", "http://hipertextual.com/software", HipertextualScraper())
+           , Website("Slashdot", "http://software.slashdot.org", SlashDotScraper())
+           , Website("UnoCero", "https://www.unocero.com/", UnoCeroScraper())
+           , Website("ACM Communication", "http://cacm.acm.org/news", HipertextualScraper())
+           ]
+
+def print_websites(websites):
+    number_website = 1
+    for website in websites:
+        print(str(number_website) + ".- " + website.name)
+        number_website += 1
+
+def main():
+    print("List of Websites:")
+    print_websites(websites)
+    selected_website = int(input("\nSelect a website:\n>>>"))
+    (titles, urls) = websites[selected_website - 1].scrap()
+
+    for title, url in zip(titles, urls):
+        print(title + "\n -> " + url)
+
+if __name__=="__main__":
+    main()
